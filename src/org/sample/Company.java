bbc8e014407dd77445046ed6fe0df444d67dce12
Sivapraba <sivasun2709@gmail.com>
2022-02-01 13:30:20 +0530

    karthi done

--- src/org/sample/Company.java
+++ src/org/sample/Company.java
@@ -1,6 +1,9 @@
 package org.sample;
 
 public class Company {
+	private void m1() {
+		System.out.println("m1 is created by karthi");
+	}
 	public static void main(String[] args) {
 		int a=10;
 		System.out.println("a value is:"+a);
