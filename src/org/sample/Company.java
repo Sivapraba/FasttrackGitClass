d76186a4de57dbaded6ea93e8efc781d00503cdd
Sivapraba <sivasun2709@gmail.com>
2022-02-01 13:36:34 +0530

    siva done

--- src/org/sample/Company.java
+++ src/org/sample/Company.java
@@ -1,6 +1,9 @@
 package org.sample;
 
 public class Company {
+	private void m2() {
+		System.out.println("m2 method is created by siva");
+	}
 	public static void main(String[] args) {
 		int a=10;
 		System.out.println("a value is:"+a);
